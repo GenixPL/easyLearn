97dfe76148fbaff8cfb9e026d887f90b57d79875
Łukasz Niedziałek <intergenixyt@gmail.com>
2018-11-16 17:56:25 +0100

    Add sets menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SetsMenuComponent } from './sets-menu/sets-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    SetsMenuComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sets-menu/sets-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sets-menu',
+  templateUrl: './sets-menu.component.html',
+  styleUrls: ['./sets-menu.component.css']
+})
+export class SetsMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
