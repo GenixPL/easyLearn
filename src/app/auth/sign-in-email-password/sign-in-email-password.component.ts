6ed791f01395223ce98742f7b5e75edd40d659fb
Łukasz Niedziałek <intergenixyt@gmail.com>
2018-12-03 18:01:33 +0100

    Add signing in by email and password

--- src/app/auth/sign-in-email-password/sign-in-email-password.component.ts
+++ src/app/auth/sign-in-email-password/sign-in-email-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Page } from 'ui/page';
 import * as firebase from 'nativescript-plugin-firebase/app';
+import * as Toast from 'nativescript-toast';
 
 
 @Component({
@@ -11,15 +12,25 @@ import * as firebase from 'nativescript-plugin-firebase/app';
 
 export class SignInEmailPasswordComponent implements OnInit {
 
+	private email: string = "dupa@o2.pl";
+	private password: string = "gfhffgh";
+
 	constructor(private page:Page) { 
 		page.actionBarHidden = true;
-
-		firebase
-			.auth().signInWithEmailAndPassword('dupa@gmail.com', 'firebase')
-			.then(() => console.log("User logged in"))
-    		.catch(err => console.log("Login error: " + JSON.stringify(err)));
 	}
 
-  	ngOnInit() { }
+	ngOnInit() { }
+	  
+	signInUser() { //TODO: make it more safe
+		firebase
+			.auth().signInWithEmailAndPassword(this.email, this.password)
+			.then((result) => { //here may be no return
+				console.log(`User signed in ${result}`); 
+			})
+			.catch((err) => {
+				let errMassage:string = JSON.stringify(err);
+				console.log("User signing error: " + errMassage);
+			});
+	}
 
 }
