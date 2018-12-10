f6e3c42a0b6905d8dade42eaf0e2b51810755e93
Łukasz Niedziałek <intergenixyt@gmail.com>
2018-12-10 09:06:34 +0100

    Change then to await in auth

--- src/app/auth/auth.component.tns.ts
+++ src/app/auth/auth.component.tns.ts
@@ -15,12 +15,12 @@ import { User } from 'nativescript-plugin-firebase';
 
 export class AuthComponent implements OnInit {
 
-	constructor(private router:Router, private page:Page) { 
+	constructor(private router: Router, private page: Page) {
 		page.actionBarHidden = true;
 	}
 
 	ngOnInit() { }
-	  
+
 	moveToSignInEmailPasswordComponent() {
 		this.router.navigate(['sign-in-email-password']);
 	}
@@ -39,26 +39,27 @@ export class AuthComponent implements OnInit {
 
 	signOut() {
 		firebase.logout()
-      		.then(() => console.log("User logged out"))
-      		.catch(error => console.log("User logging out error: " + JSON.stringify(error)));
+			.then(() => console.log("User logged out"))
+			.catch(error => console.log("User logging out error: " + JSON.stringify(error)));
 	}
 
-	getCurrentUser() {
-		firebase.getCurrentUser()
-		.then((user) => {
+	async getCurrentUser() {
+		try {
+			let user: User = await firebase.getCurrentUser();
+
 			if (isDefined(user)) {
 				let userInfo: string = `${user.uid} ${user.email}`;
 
 				Toast.makeText(`Current user: ${userInfo}`, `long`).show();
 				console.log(`Current user: ${userInfo}`);
-			
+
 			} else {
-				Toast.makeText(`Current user is undefined`, `long`).show(); //TODO: sth is wrong and users are not save over sessions
+				Toast.makeText(`Current user is undefined`, `long`).show();
 				console.log(`Current user is undefined`);
 			}
-
-		}).catch((err) => {
+			
+		} catch (err) {
 			console.log(`Error during getting current user: ${err}`);
-		});
+		}
 	}
 }
--- src/app/auth/email-password-auth/sign-up-email-password/sign-up-email-password.component.tns.ts
+++ src/app/auth/email-password-auth/sign-up-email-password/sign-up-email-password.component.tns.ts
@@ -4,6 +4,7 @@ import * as Toast from 'nativescript-toast';
 import { Page } from 'tns-core-modules/ui/page/page';
 
 import { createFilesForNewUser } from '../../../firebase-functions/create-user-files';
+import { User } from 'nativescript-plugin-firebase';
 
 
 @Component({
@@ -17,29 +18,29 @@ export class SignUpEmailPasswordComponent implements OnInit {
 	private email: string = "ciekaw@o2.pl";
 	private password: string = "asdasdasd";
 
-	constructor(private page:Page) { 
+	constructor(private page: Page) {
 		page.actionBarHidden = true;
 	}
 
 	ngOnInit() { }
-	  
-	signUpNewUser() { //TODO: make it more safe
-		firebase.createUser({
-		  	email: this.email,
-		  	password: this.password
 
-		}).then((user) => {
+	async signUpNewUser() { //TODO: make it more safe
+		try {
+			let user: User = await firebase.createUser({
+				email: this.email,
+				password: this.password
+			});
 			console.log(`New user created through emial-password auth: ${JSON.stringify(user)}`);
 			createFilesForNewUser(user);
 
-		}).catch((err) => {
+		} catch(err) {
 			console.log(`Error occured during user creation with email-password auth: ${err}`);
 
-			let errMassage:string = JSON.stringify(err);
+			let errMassage: string = JSON.stringify(err);
 			if (errMassage.includes("FirebaseAuthUserCollisionException")) {
-    			Toast.makeText(`ERROR: Account with the mail: ${this.email} already exists.`, "long").show();
+				Toast.makeText(`ERROR: Account with the mail: ${this.email} already exists.`, "long").show();
 			}
-		});
+		}
 	}
 
 }
